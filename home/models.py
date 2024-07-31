d8b39aece1d46bb278db283da854c4284e621a85
Serkan <serkanbulutgenc@gmail.com>
2024-07-31 19:53:17 +0000

    Created base homepage

--- home/models.py
+++ home/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField 
 
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True, help_text="Body field for Home page")
+
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body')
+    ]
