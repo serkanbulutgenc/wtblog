8e2f226cbc4738067bfd772acd90e7c1b5fee456
Serkan <serkanbulutgenc@gmail.com>
2024-07-31 19:38:58 +0000

    Initial commit

---
+++ wtblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-n5$zllv!f^m71g)_^p4k&8ixq2^7-rusc8m69oii7o15t-j2(@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
